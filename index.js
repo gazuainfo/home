4411d611808a98feff75aaa8dc9537d201768904
yanghyunjin <yang.skkuice@gmail.com>
2018-01-28 19:05:46 +0900

    asdf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
  
-app.use(express.static(__dirname + `/public`))
+app.use(express.static(`./public`))
 console.log("server start");
 
 app.listen(3000)
