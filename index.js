5daf4174458a54b74425528246a1b6276e2ca801
yanghyunjin <yang.skkuice@gmail.com>
2018-01-28 21:10:17 +0900

    test express

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express')
+var path = require('path')
 var app = express()
  
-app.use(express.static(`./public`))
+app.use(express.static(__dirname + `/public`))
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 console.log("server start");
 
-app.listen(9000)
+app.listen(3000)
